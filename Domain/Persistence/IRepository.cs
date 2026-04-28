e8694c9ea6611ad7645a1af7cc0b620e9601e262
chr005-dev <chr005@edu.zealand.dk>
2026-04-28 12:09:21 +0200

    Fix fejl 40 omkring IRepository

--- Domain/Persistence/IRepository.cs
+++ Domain/Persistence/IRepository.cs
@@ -1,7 +1,15 @@
-﻿namespace Domain.Persistence
-{
-    public class IRepository
-    {
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
+namespace Domain.Persistence
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T? GetById(Guid id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(Guid id);
     }
 }
