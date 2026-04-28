0a5aa72164b249c85a60054f62cb25b6ff602b16
chr005-dev <chr005@edu.zealand.dk>
2026-04-28 10:55:57 +0200

    Tilføjet mapper og vores IRepository

---
+++ Domain/Persistence/IRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Persistence
+{
+    public class IRepository
+    {
+
+    }
+}
---
+++ Infrastructure/Data/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.Emit;
+
+namespace Infrastructure.Data
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T? GetById(Guid id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(Guid id);
+    }
+}
