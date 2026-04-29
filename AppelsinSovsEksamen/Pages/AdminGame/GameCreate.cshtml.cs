db6f83f5095f8f663b65b4b3b8eb5ce2687f7307
chr005-dev <chr005@edu.zealand.dk>
2026-04-29 11:51:04 +0200

    Flere mapper yippee !!!!!!

---
+++ AppelsinSovsEksamen/Pages/AdminGame/GameCreate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppelsinSovsEksamen.Pages.AdminGame
+{
+    public class AdminGameCreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ AppelsinSovsEksamen/Pages/AdminProduct/ProductCreate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppelsinSovsEksamen.Pages.AdminProduct
+{
+    public class ProductCreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
