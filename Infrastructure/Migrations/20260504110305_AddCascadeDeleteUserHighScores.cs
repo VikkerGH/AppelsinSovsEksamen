8ab7f3054ada191e8317fc89672c6fac2ce7a296
Sebahattin Emre Türkoglu <set003@edu.zealand.dk>
2026-05-04 13:19:08 +0200

    fjernet ændringer

---
+++ Infrastructure/Migrations/20260504110305_AddCascadeDeleteUserHighScores.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCascadeDeleteUserHighScores : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 5, 4, 11, 3, 4, 917, DateTimeKind.Utc).AddTicks(1209));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 4, 30, 10, 58, 22, 521, DateTimeKind.Utc).AddTicks(5149));
+        }
+    }
+}
