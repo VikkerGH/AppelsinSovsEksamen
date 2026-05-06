b67053b43074cc8d5866900fd0d31e5462f6e9c4
Sebahattin Emre Türkoglu <set003@edu.zealand.dk>
2026-05-06 12:22:35 +0200

    Jeg er færdig med User Story SN2-26

--- AppelsinSovsEksamen/Pages/UserSides/UserEdit.cshtml.cs
+++ AppelsinSovsEksamen/Pages/UserSides/UserEdit.cshtml.cs
@@ -3,25 +3,29 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AppelsinSovsEksamen.Pages.User
 {
-    public class IndexModel : PageModel
+    public class UserEditModel : PageModel
     {
         private readonly Domain.Services.UserService _userService;
 
-        public IndexModel(Domain.Services.UserService userService)
+        public UserEditModel(Domain.Services.UserService userService)
         {
             _userService = userService;
         }
-
-        [BindProperty(SupportsGet = true)]
-        public Guid Id { get; set; }
 
         public Domain.Models.User? CurrentUser { get; set; }
 
         public IActionResult OnGet()
         {
+            var userIdString = HttpContext.Session.GetString("UserId");
+
+            if (string.IsNullOrEmpty(userIdString) || !Guid.TryParse(userIdString, out Guid userId))
+            {
+                return RedirectToPage("/UserSides/UserLogIn");
+            }
+
             try
             {
-                CurrentUser = _userService.GetById(Id);
+                CurrentUser = _userService.GetById(userId);
                 return Page();
             }
             catch (ArgumentException)
