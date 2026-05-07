cca632b6ed5d96b4b0efc33d0301b7a417305ce0
SebD111 <sed006@edu.zealand.dk>
2026-05-07 15:04:39 +0200

    Færdig med user story: slette product/merch

--- AppelsinSovsEksamen/Pages/AdminProduct/ProductDelete.cshtml.cs
+++ AppelsinSovsEksamen/Pages/AdminProduct/ProductDelete.cshtml.cs
@@ -1,12 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Domain.Models;
+using Domain.Services;
+using System;
 
 namespace AppelsinSovsEksamen.Pages.AdminProduct
 {
     public class ProductDeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly ProductService _productService;
+
+        public ProductDeleteModel(ProductService productService)
         {
+            _productService = productService;
+        }
+
+        [BindProperty]
+        public Domain.Models.Product ProductToDelete { get; set; } = new Domain.Models.Product();
+
+        public IActionResult OnGet(Guid id)
+        {
+            var product = _productService.GetById(id);
+
+            if (product == null)
+            {
+                return RedirectToPage("./ProductIndex");
+            }
+
+            ProductToDelete = product;
+            return Page();
+        }
+
+        public IActionResult OnPost(Guid id)
+        {
+            _productService.Delete(id);
+
+            return RedirectToPage("./ProductIndex");
         }
     }
 }
