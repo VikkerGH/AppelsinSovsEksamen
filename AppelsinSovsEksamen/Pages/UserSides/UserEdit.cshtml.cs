455558c750970c3f646ddfabf0afef6b8ee178ee
Sebahattin Emre Türkoglu <set003@edu.zealand.dk>
2026-05-08 09:55:18 +0200

    Færdig med User Story SN2-42

--- AppelsinSovsEksamen/Pages/UserSides/UserEdit.cshtml.cs
+++ AppelsinSovsEksamen/Pages/UserSides/UserEdit.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace AppelsinSovsEksamen.Pages.User
 {
@@ -14,6 +15,18 @@ namespace AppelsinSovsEksamen.Pages.User
 
         public Domain.Models.User? CurrentUser { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public Guid Id { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Navn er påkrævet")]
+        public string Name { get; set; } = string.Empty;
+
+        [BindProperty]
+        [StringLength(100, MinimumLength = 6,
+     ErrorMessage = "Kodeord skal være mindst 6 tegn")]
+        public string? Password { get; set; }
+
         public IActionResult OnGet()
         {
             var userIdString = HttpContext.Session.GetString("UserId");
@@ -26,12 +39,33 @@ namespace AppelsinSovsEksamen.Pages.User
             try
             {
                 CurrentUser = _userService.GetById(userId);
+
+                // Udfyld felterne
+                Name = CurrentUser.Name;
+
                 return Page();
             }
             catch (ArgumentException)
             {
                 return NotFound();
             }
+        }
+
+        public IActionResult OnPost()
+        {
+            var userIdString = HttpContext.Session.GetString("UserId");
+            if (string.IsNullOrEmpty(userIdString) || !Guid.TryParse(userIdString, out Guid userId))
+            {
+                return RedirectToPage("/UserSides/UserLogIn");
+            }
+
+            if (!ModelState.IsValid)
+                return Page();
+
+            _userService.Edit(userId, Name, Password);
+            HttpContext.Session.SetString("UserName", Name);
+            TempData["Success"] = "Dine oplysninger er blevet opdateret.";
+            return RedirectToPage("/UserSides/UserEdit");
         }
     }
 }
