aca065de676df5b20a68f9dafa804b3456e9519e
SebD111 <sed006@edu.zealand.dk>
2026-05-18 13:28:47 +0200

    Lavet task vedr. crud af spil

--- AppelsinSovsEksamen/Pages/AdminGame/GameCreate.cshtml.cs
+++ AppelsinSovsEksamen/Pages/AdminGame/GameCreate.cshtml.cs
@@ -1,12 +1,46 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Domain.Services;
 
 namespace AppelsinSovsEksamen.Pages.AdminGame
 {
-    public class AdminGameCreateModel : PageModel
+    public class GameCreateModel : PageModel
     {
-        public void OnGet()
+        private readonly GameService _gameService;
+
+        public GameCreateModel(GameService gameService)
         {
+            _gameService = gameService;
+        }
+
+        [BindProperty]
+        public Domain.Models.Game NewGame { get; set; } = new Domain.Models.Game();
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var alleSpil = _gameService.GetAll();
+
+            bool spilFindes = alleSpil.Any(g => g.Name.ToLower() == NewGame.Name.ToLower());
+
+            if (spilFindes)
+            {
+                ModelState.AddModelError("NewGame.Name", "Der findes allerede et spil med dette navn.");
+                return Page();
+            }
+
+            _gameService.Create(NewGame.Name, NewGame.Description);
+
+            return RedirectToPage("./GameIndex");
         }
     }
 }
--- AppelsinSovsEksamen/Pages/AdminGame/GameDelete.cshtml.cs
+++ AppelsinSovsEksamen/Pages/AdminGame/GameDelete.cshtml.cs
@@ -1,12 +1,49 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Domain.Services;
+using Domain.Models;
+using System;
 
 namespace AppelsinSovsEksamen.Pages.AdminGame
 {
-    public class AdminGameDeleteModel : PageModel
+    public class GameDeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly GameService _gameService;
+
+        public GameDeleteModel(GameService gameService)
         {
+            _gameService = gameService;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public Guid Id { get; set; }
+
+        public Domain.Models.Game? GameToDelete { get; set; }
+
+        public IActionResult OnGet()
+        {
+            try
+            {
+                GameToDelete = _gameService.GetById(Id);
+                return Page();
+            }
+            catch (ArgumentException)
+            {
+                return RedirectToPage("./GameIndex");
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            try
+            {
+                _gameService.Delete(Id);
+                return RedirectToPage("./GameIndex");
+            }
+            catch (ArgumentException)
+            {
+                return RedirectToPage("./GameIndex");
+            }
         }
     }
 }
